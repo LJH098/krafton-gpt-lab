6a2442e360d08c735694920b5db25bef63204d27
Jin Hyuk <ekrrdj07@gmail.com>
2026-06-01 14:49:50 +0900

    feat: GELU forward

--- src/model.py
+++ src/model.py
@@ -34,7 +34,8 @@ class GELU(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """TODO: tanh 근사식 또는 torch 연산으로 GELU를 구현합니다."""
-        raise NotImplementedError("GELU.forward를 구현하세요.")
+        return 0.5 * x * (1 + torch.tanh(torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
+                                         (x + 0.044715 * torch.pow(x,3))))
 
 
 class FeedForward(nn.Module):
