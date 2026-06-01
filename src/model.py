7941e7e81424a11163d720e587382797e251e946
Jin Hyuk <ekrrdj07@gmail.com>
2026-06-01 16:17:34 +0900

    fix: GPTModel

--- src/model.py
+++ src/model.py
@@ -101,16 +101,24 @@ class GPTModel(nn.Module):
         super().__init__()
         self.config = config
         # TODO: embedding, blocks, final layernorm, lm_head를 정의하세요.
-        self.tok_emb = nn.Embedding(config["vocab_size"], config["emb_dim"])
-        self.pos_emb = nn.Embedding(config["context_length"], config["emb_dim"])
-        self.drop_emb = nn.Dropout(config["drop_rate"])
+        emb_dim = self.config["emb_dim"]
+        n_heads = self.config["n_heads"]
+        drop_rate = self.config["drop_rate"]
+        qkv_bias = self.config["qkv_bias"]
+        vocab_size = self.config["vocab_size"]
         
+        self.tok_emb = nn.Embedding(vocab_size, emb_dim)
+        self.pos_emb = nn.Embedding(config["context_length"], emb_dim)
+        self.drop_emb = nn.Dropout(drop_rate)
+        
+   
+         
         self.trf_blocks = nn.Sequential(
-            *[TransformerBlock(config) for _ in range(config["n_layers"])]
+            *[TransformerBlock(emb_dim, n_heads, drop_rate, qkv_bias) for _ in range(config["n_layers"])]
         )
-        self.final_norm = LayerNorm(config["emb_dim"])
+        self.final_norm = LayerNorm(emb_dim)
         self.out_head = nn.Linear(
-            config["emb_dim"], config["vocab_size"], bias=False
+            config[emb_dim], vocab_size, bias=False
         )
 
     def forward(
@@ -137,9 +145,9 @@ class GPTModel(nn.Module):
         x = self.final_norm(x)
         logits = self.out_head(x)
         
-        if targets:
+        if targets != None:
             loss = nn.functional.cross_entropy(
-                logits.flatten(0,1), targets.flatten)
+                logits.flatten(0,1), targets.flatten())
             return loss, logits
         return logits
 
